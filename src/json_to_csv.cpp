81fc98cc2871de439f3844551d4a787a1c09bb7e
Gianmaria <gianmaria.bonici@gmail.com>
2022-10-23 17:25:31 +0200

    parsing iso duration

--- src/json_to_csv.cpp
+++ src/json_to_csv.cpp
@@ -1,5 +1,26 @@
 #include "pch.h"
 
+struct ISO_Duration
+{
+    uint32_t H;
+    uint32_t M;
+    uint32_t S;
+};
+
+ISO_Duration parse_ISO_duration(string_view duration)
+{
+    // example: PT1H3M13S
+    ISO_Duration res{};
+
+    if (duration[0] != 'T')
+    {
+        auto msg = std::format("Invalid duration: {}"sv, duration);
+        throw std::runtime_error(msg);
+    }
+
+    return res;
+}
+
 string UTC_to_excel(const std::string& input)
 {
     std::istringstream ss(input);
@@ -22,13 +43,22 @@ string UTC_to_excel(const std::string& input)
 
 int main()
 {
-    date::set_install(".\\data\\tzdata2022e");
-    auto& tzdb = date::get_tzdb();
-    std::cout << "tzdb version: " << tzdb.version << endl;
+    try
+    {
+        date::set_install(".\\data\\tzdata2022e");
+        auto& tzdb = date::get_tzdb();
+        std::cout << "tzdb version: " << tzdb.version << endl;
 
-    auto input = "2022-10-14T18:11:16Z";
-    cout << "input:  " << input << endl;
-    cout << "output: " << UTC_to_excel(input) << endl;
+        auto input = "2022-10-14T18:11:16Z";
+        cout << "input:  " << input << endl;
+        cout << "output: " << UTC_to_excel(input) << endl;
 
-    return 0;
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        cout << e.what() << endl;
+    }
+
+    return 1;
 }
--- src/pch.h
+++ src/pch.h
@@ -7,11 +7,16 @@
 #include <string_view>
 #include <vector>
 #include <chrono>
+#include <exception>
+#include <format>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::string_view;
 using namespace std::string_view_literals;
+using namespace std::string_literals;
+
 namespace fs = std::filesystem;
 
 #include "date/date.h"
